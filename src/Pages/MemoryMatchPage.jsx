86dce46eb2850ef52f7e719a9321b6a8b6f646d9
Javier Moreno <javiermorenov1203@gmail.com>
2025-10-16 01:02:14 -0300

    memory match v0.3

--- src/Pages/MemoryMatchPage.jsx
+++ src/Pages/MemoryMatchPage.jsx
@@ -1,17 +1,48 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import GameTitle from "../components/GameTitle"
 import RestartButton from "../components/RestartButton"
 import './MemoryMatchPage.css'
 
 export default function MemoryMatchPage() {
 
-    const [cards, setCards] = useState(['🐕', '🐕', '🐕', '🐕', '🐕', '🐕', '🐕', '🐕', '🐕', '🐕'])
+    const firstAttempt = useRef(true);
+    const firstAttemptIcon = useRef('');
+    const firstAttemptIndex = useRef(null);
+    const onWait = useRef(false);
+
+    const [cards, setCards] = useState(['🐕', '🐕', '🐕', '🐕', '🐕', '🍉', '🍉', '🍉', '🍉', '🍉'])
     const [isHidden, setIsHidden] = useState(Array(10).fill(true))
 
     function handleClick(index) {
+        if (!isHidden[index] || onWait.current) return;
+
         const newHidden = [...isHidden];
         newHidden[index] = false;
         setIsHidden(newHidden);
+
+        if (firstAttempt.current) {
+            firstAttemptIcon.current = cards[index]
+            firstAttemptIndex.current = index
+        } else {
+            if (cards[index] !== firstAttemptIcon.current) {
+
+                onWait.current = true
+                const firstIndex = firstAttemptIndex.current;
+                
+                setTimeout(() => {
+                    const updatedHidden = [...newHidden];
+                    updatedHidden[index] = true;
+                    updatedHidden[firstIndex] = true;
+                    setIsHidden(updatedHidden)
+                    onWait.current = false
+                }, 1000);
+            }
+
+            firstAttemptIcon.current = ''
+            firstAttemptIndex.current = null
+        }
+
+        firstAttempt.current = !firstAttempt.current
     }
 
     return (
@@ -20,12 +51,17 @@ export default function MemoryMatchPage() {
             <p>Select the cards and find the matching pairs.</p>
             <div className="card-container">
                 {cards.map((value, index) => (
-                    <button className={`card ${isHidden[index] ? 'hidden-card' : ''}`} onClick={() => handleClick(index)}>
+                    <button key={index} className={`card ${isHidden[index] ? 'hidden-card' : ''}`} onClick={() => handleClick(index)}>
                         <span className="icon">{isHidden[index] ? '❔' : value}</span>
                     </button>
                 ))}
             </div>
-            <RestartButton onClick={() => setIsHidden(Array(10).fill(true))}></RestartButton>
+            <RestartButton onClick={() => {
+                setIsHidden(Array(10).fill(true))
+                firstAttempt.current = true;
+                firstAttemptIcon.current = '';
+                firstAttemptIndex.current = null;
+            }}></RestartButton>
         </>
     )
 }
