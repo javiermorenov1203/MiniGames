93b3493abda12d3a0519661e56c4b9fee6ad86f2
Javier Moreno <javiermorenov1203@gmail.com>
2025-10-15 21:28:12 -0300

    added memory match button

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,7 +7,8 @@ function HomePage() {
     <>
       <h1>MiniGames Page</h1>
       <p>Welcome to the MiniGames Homepage. <br />Select the game you want to play.</p>
-      <GameButton icon={'❌'} gameName={'Tic Tac Toe'}></GameButton>
+      <GameButton icon={'❌'} gameName={'Tic Tac Toe'} redirection={'TicTacToe'}></GameButton>
+      <GameButton icon={'🧠'} gameName={'Memory Match'}></GameButton>
     </>
   )
 }
--- src/components/GameButton.jsx
+++ src/components/GameButton.jsx
@@ -1,11 +1,11 @@
 import { useNavigate } from 'react-router-dom'
 import './GameButton.css'
 
-export function GameButton({ icon, gameName }) {
+export function GameButton({ icon, gameName, redirection }) {
     const navigate = useNavigate()
     return (
         <div className='game-buttons-container'>
-            <button className='game-button' onClick={() => navigate('/TicTacToe')}>{icon}</button>
+            <button className='game-button' onClick={() => navigate(`/${redirection}`)}>{icon}</button>
             <p className='game-title'>{gameName}</p>
         </div>
     )
