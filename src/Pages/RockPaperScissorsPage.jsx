737206af3194de2cdabe06f3d1c4a0c082967cf6
Javier Moreno <javiermorenov1203@gmail.com>
2025-10-22 00:12:52 -0300

    rock paper scissor v1.0

--- src/Pages/RockPaperScissorsPage.jsx
+++ src/Pages/RockPaperScissorsPage.jsx
@@ -1,30 +1,38 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import GameTitle from "../components/GameTitle"
 import RestartButton from "../components/RestartButton"
 import './RockPaperScissorsPage.css'
 
 export default function RockPaperScissorsPage() {
 
+    const myOption = useRef(null)
+    const oponentOption = useRef(null)
+
+    const myScore = useRef(0)
+    const oponentScore = useRef(0)
+
+    const [myScoreVisual, setMyScoreVisual] = useState(0)
+    const [oponentScoreVisual, setOponentScoreVisual] = useState(0)
+
 
     const [optionSelected, setOptionSelected] = useState('')
     const [oponentOptionSelected, setOponentOptionSelected] = useState('')
     const options = ['👊', '✌️', '✋']
 
-    function handlePlay() {
+    const onWait = useRef(false)
 
-        let timeOut = 100
+    function handlePlay() {
+        if (onWait.current === true) {
+            return
+        }
+        onWait.current = true
+        let timeOut = 0
+        let index = 0
 
         while (timeOut < 1000) {
             setTimeout(() => {
-                setOponentOptionSelected(options[0])
-            }, timeOut);
-            timeOut += 100
-            setTimeout(() => {
-                setOponentOptionSelected(options[1])
-            }, timeOut);
-            timeOut += 100
-            setTimeout(() => {
-                setOponentOptionSelected(options[2])
+                setOponentOptionSelected(options[index])
+                index = (index + 1) % 3
             }, timeOut);
             timeOut += 100
         }
@@ -32,18 +40,54 @@ export default function RockPaperScissorsPage() {
         setTimeout(() => {
             const oponetSelection = options[Math.floor(Math.random() * 3)]
             setOponentOptionSelected(oponetSelection)
-            const mySelection = document.getElementById('my-option').textContent
+            const mySelection = myOption.current.textContent
             if (mySelection === oponetSelection) {
-                console.log('Empate')
+                myOption.current.style.backgroundColor = 'rgb(116, 116, 116)';
+                oponentOption.current.style.backgroundColor = 'rgb(116, 116, 116)';
             } else if (mySelection === '👊') {
                 if (oponetSelection === '✌️') {
-                    console.log('Has ganado')
+                    myOption.current.style.backgroundColor = 'rgb(0, 169, 0)'
+                    myScore.current += 1
                 } else {
-                    console.log('Has perdido')
+                    oponentOption.current.style.backgroundColor = 'rgb(0, 169, 0)'
+                    oponentScore.current += 1
+                }
+            } else if (mySelection === '✌️') {
+                if (oponetSelection === '👊') {
+                    oponentOption.current.style.backgroundColor = 'rgb(0, 169, 0)'
+                    oponentScore.current += 1
+                } else {
+                    myOption.current.style.backgroundColor = 'rgb(0, 169, 0)'
+                    myScore.current += 1
+                }
+            } else if (mySelection === '✋') {
+                if (oponetSelection === '👊') {
+                    myOption.current.style.backgroundColor = 'rgb(0, 169, 0)'
+                    myScore.current += 1
+                } else {
+                    oponentOption.current.style.backgroundColor = 'rgb(0, 169, 0)'
+                    oponentScore.current += 1
                 }
             }
-        }, 1000);
 
+            setTimeout(() => {
+                setOponentOptionSelected('')
+                setOptionSelected('')
+                myOption.current.style.backgroundColor = 'rgb(221, 221, 221)'
+                oponentOption.current.style.backgroundColor = 'rgb(221, 221, 221)'
+                setMyScoreVisual(myScore.current)
+                setOponentScoreVisual(oponentScore.current)
+                onWait.current = false
+            }, 1000);
+
+        }, 1000);
+    }
+
+    function handleOptionSeleted(value) {
+        if (onWait.current === true) {
+            return
+        }
+        setOptionSelected(value)
     }
 
     return (
@@ -55,14 +99,14 @@ export default function RockPaperScissorsPage() {
                     <div className="rock-paper-scissor-left-panel">
                         <div className="rps-score-counter">
                             <span className="score-title">Score</span>
-                            <span className="score-number">1</span>
+                            <span className="score-number">{myScoreVisual}</span>
                         </div>
                         <div className="rock-paper-scissor-option-container">
-                            <button className="rock-paper-scissor-option" onClick={() => setOptionSelected('👊')}>👊</button>
-                            <button className="rock-paper-scissor-option" onClick={() => setOptionSelected('✌️')}>✌️</button>
-                            <button className="rock-paper-scissor-option" onClick={() => setOptionSelected('✋')}>✋</button>
+                            <button className="rock-paper-scissor-option" onClick={() => handleOptionSeleted('👊')}>👊</button>
+                            <button className="rock-paper-scissor-option" onClick={() => handleOptionSeleted('✌️')}>✌️</button>
+                            <button className="rock-paper-scissor-option" onClick={() => handleOptionSeleted('✋')}>✋</button>
                         </div>
-                        <div id="my-option" className="rock-paper-scissor-option-selected">
+                        <div ref={myOption} className="rock-paper-scissor-option-selected">
                             {optionSelected}
                         </div>
                     </div>
@@ -74,19 +118,26 @@ export default function RockPaperScissorsPage() {
                 <div className="rock-paper-scissor-right-container">
                     <h3 className="rock-paper-scissor-player-title">Oponent</h3>
                     <div className="rock-paper-scissor-right-panel">
-                        <div id="oponent-option" className="rock-paper-scissor-option-selected">
+                        <div ref={oponentOption} className="rock-paper-scissor-option-selected">
                             {oponentOptionSelected}
                         </div>
                         <div className="rps-score-counter">
                             <span className="score-title">Score</span>
-                            <span className="score-number">1</span>
+                            <span className="score-number">{oponentScoreVisual}</span>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="rps-button-container">
                 <button onClick={() => handlePlay()} id="play-button" disabled={!optionSelected}>Play</button>
-                <RestartButton ></RestartButton>
+                <RestartButton onClick={() => {
+                    myScore.current = 0
+                    oponentScore.current = 0
+                    setMyScoreVisual(0)
+                    setOponentScoreVisual(0)
+                    setOptionSelected('')
+                    setOponentOptionSelected('')
+                }}></RestartButton>
             </div>
         </>
     )
